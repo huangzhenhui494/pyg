d390e66064bd6297c7cd7637d0fe900cf2158fb5
huangzhenhui494 <hzhloveymt968@gmail.com>
2019-01-03 22:56:54 +0800

    第一次把maven项目提交到GitHub

---
+++ pyg-manager/pyg-manager-interface/src/main/java/com/pyg/manager/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pyg.manager.service;
+
+import java.util.List;
+
+import com.pyg.pojo.TbBrand;
+
+public interface BrandService {
+
+	/**
+	 * 需求:查询所有品牌数据
+	 */
+	public List<TbBrand> findAll();
+}
